3bc54ef8dc4852513c620c0f2732e273f6d79dbd
mmskv <max.mishuk2014@gmail.com>
2023-09-29 02:32:14 +0300

    feat(wg): set up wg0 interface in entrypoint

--- wg/entrypoint.sh
+++ wg/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 ip link add dev wg0 type wireguard
+ip addr add 10.69.69.1/24 dev wg0
+ip link set wg0 up
+iptables -A FORWARD -i wg0 -j ACCEPT
+iptables -t nat -A POSTROUTING -s 10.69.69.0/24 -o eth0 -j MASQUERADE
 
 /usr/local/bin/wgrpc
