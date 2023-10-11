ec569ca792926f37795bd89615a4e66458be830c
mmskv <max.mishuk2014@gmail.com>
2023-10-11 05:06:14 +0300

    fix(keys): don't require scope in a JWT

--- keys/src/jwt.rs
+++ keys/src/jwt.rs
@@ -22,17 +22,22 @@ impl JwtValidator {
     }
 
     pub fn validate(&self, request_username: &str, metadata: &MetadataMap) -> Result<(), Status> {
-        let token_str = metadata
+        let (name, token_str) = metadata
             .get("authorization")
             .ok_or_else(|| Status::unauthenticated("Missing authorization header"))?
             .to_str()
-            .map_err(|e| Status::unauthenticated(format!("Invalid authorization header: {}", e)))?;
+            .map_err(|e| Status::unauthenticated(format!("Invalid authorization header: {}", e)))?
+            .split_once(" ")
+            .ok_or_else(|| Status::unauthenticated("Invalid authorization header"))?;
+
+        if name != "Bearer" {
+            return Err(Status::unauthenticated("Missing Bearer token"));
+        }
 
         let token = jwt::JwtRef::from_str(token_str);
-
         let claims: Claims = self
             .authority
-            .verify_token(token, &oauth::ScopePolicy::default())
+            .verify_token(token, &oauth::ScopePolicy::allow_any())
             .map_err(|e| Status::unauthenticated(format!("Token verification failed: {}", e)))?;
 
         if request_username == claims.username {
